841da064873de809d09f8f01c5d2d2102352251c
Sai Yashwanth <taddishetty34@gmail.com>
2024-03-11 16:48:59 +0530

    Add: complete code on demand - Done. Need to format string a little bitter and a lot of bugs needs to be addressed

--- lua/plugins/intelli.lua
+++ lua/plugins/intelli.lua
@@ -14,14 +14,14 @@ return {
                 
                 -- Get current buffer code. Need to change so that only till the cursor gets extracted
                 local buf_content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-                local code = table.concat(buf_content, "\n")
+                local code = table.concat(buf_content)
 
                 -- local api_url = "https://api-inference.huggingface.co/models/google/gemma-7b"
                 local api_url = "https://api-inference.huggingface.co/models/codellama/CodeLlama-7b-hf"
                 local auth_token = "YOUR-API-KEY"
 
                 local payload = {
-                    inputs = "Complete the following code snippet. give code only. Give code only after the input: " .. code
+                    inputs = "Complete the following code snippet. give code only, dont give other answer. : " .. code
                 }
 
                 local payload_json = vim.fn.json_encode(payload)
@@ -43,20 +43,46 @@ return {
                 local response = vim.fn.systemlist(cmd)
                 -- Write it to a file to debug
                 
-                local last_line
+                local last_line = ""
                 for _, line in ipairs(response) do
                     last_line = line
                 end
-                local input = "Complete the following code snippet. give code only. Give code only after the input: " .. code
-
-               
+                -- local input = "Complete the following code snippet. give code only. Give code only after the input: " .. code       
                 -- local response_file = io.open("./response.txt", "w")
                 -- if response_file then
                 --     response_file:write(output_lines .. "\n")
                 --     response_file:close()
                 -- end
+                local output = {}
+                local first_colon_index = last_line:find(':')
 
-                vim.api.nvim_buf_set_lines(0, -1, -1, false, {last_line}) 
+                if first_colon_index then
+                    local second_colon_index = last_line:find(':', first_colon_index + 1)
+                    if second_colon_index then
+                        local endline_index = last_line:find("Complete", second_colon_index)
+                        
+                        local code_part
+                        if endline_index then
+                            code_part = last_line:sub(second_colon_index + 1, endline_index - 3)
+                        else
+                            code_part = last_line:sub(second_colon_index + 1)  
+                        end
+                        local start_index, end_index = code_part:find(code, 1, true)
+
+                        if start_index then
+                            code_part = code_part:sub(1, start_index - 1) .. code_part:sub(end_index + 1)
+                        end
+                        table.insert(output, code_part)
+                    -- add an else statement to tell nothing found (as a comment) todo
+                    else
+                        table.insert(output, "None, press undo")
+                    end
+                end
+                local cursor = vim.api.nvim_win_get_cursor(0)
+                local current_line = cursor[1] - 1
+                local current_col = cursor[2]
+
+                vim.api.nvim_buf_set_lines(0, current_line+1, current_line+1, false, output)     
             end
             vim.cmd([[command! AI lua CodeComplete()]])        
         end
--- test.c
+++ test.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
 
-
 int sum(int a,
+  int b, int c, int d) { return a + b + c + d; }main () { int a = 10, b = 20, c = 30, d = 40;int sum = 0;sum = sum(a, b, c, d);printf (\"they the party by a big sum of %d. Goodbye.\\n \", sum); }
