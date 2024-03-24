14725f4d42be5bdcbb1a95ca63f573c7af9786bf
Sai Yashwanth <taddishetty34@gmail.com>
2024-03-24 19:15:29 +0530

    Add: added logic to fetch from huggingface

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyparser = require('body-parser')
-
+require('dotenv').config()
 
 app.use(bodyparser.json);
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/codellama/CodeLlama-13b-hf",
+		{
+			headers: { Authorization: `${process.env.API}` },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+
+
 
 app.get('/', async (req, res) => {
   res.send('Hello World!')
@@ -19,7 +35,15 @@ app.post('/getcode', async (req, res) => {
 })
 
 app.get('/apires', async (req, res) => {
-
+  // let rescode;
+  try{
+    const respone = await query({"inputs": "def sum(a, b): "})	  
+    console.log("Hit the endpoint")
+	  console.log(JSON.stringify(response));
+	  res.send(response)
+  } catch (error){
+    res.send('error')
+  } 
 })
 
 app.listen(port, () => {
